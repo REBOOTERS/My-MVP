ffb96fb3a01e009454304168819ba85fdec07ba5
REBOOTERS <foreverlove.zyl@163.com>
2016-11-14 08:22:48 +0800

    del useless files

--- app/src/main/java/huyifei/mymvp/MainActivity.java
+++ app/src/main/java/huyifei/mymvp/MainActivity.java
@@ -9,11 +9,12 @@ import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -26,15 +27,13 @@ import huyifei.mymvp.mvp.LoginActivity;
 import huyifei.mymvp.mvp.SimpleLoginActivity;
 import huyifei.mymvp.util.V;
 
-import static huyifei.mymvp.R.id.desc;
-
 /**
  * Created by rookie on 2016/11/2.
  */
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
     private Context mContext;
-    private ListView listView;
+    private RecyclerView recyclerView;
     private SwipeRefreshLayout swipeRefreshLayout;
     private List<ItemInfo> demos = new ArrayList<>();
 
@@ -63,16 +62,10 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         swipeRefreshLayout.setOnRefreshListener(this);
         swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent, R.color.colorPrimary);
         swipeRefreshLayout.setProgressBackgroundColorSchemeColor(Color.WHITE);
-        listView = V.f(this, R.id.list);
+        recyclerView = V.f(this, R.id.list);
         MyAdpater myAdpater = new MyAdpater();
-        listView.setAdapter(myAdpater);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(mContext, demos.get(position).activitys);
-                startActivity(intent);
-            }
-        });
+        recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        recyclerView.setAdapter(myAdpater);
 
 
     }
@@ -88,53 +81,58 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
     }
 
 
-    private class MyAdpater extends BaseAdapter {
+    private class MyAdpater extends RecyclerView.Adapter<MyAdpater.MyHolder> {
 
 
         public MyAdpater() {
+
         }
 
         @Override
-        public View getView(int index, View convertView, ViewGroup parent) {
-            Holder holder = null;
-            if (convertView == null) {
-                holder = new Holder();
-                convertView = View.inflate(mContext, R.layout.demo_info_item, null);
-                convertView.setTag(holder);
-            } else {
-                holder = (Holder) convertView.getTag();
-            }
-
-
-            convertView.setTag(holder);
-
-            holder.title = (TextView) convertView.findViewById(R.id.title);
-            holder.desc = (TextView) convertView.findViewById(desc);
-
-            holder.title.setText(demos.get(index).activitys.getSimpleName());
-            holder.desc.setText(demos.get(index).desc);
-            return convertView;
+        public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = getLayoutInflater();
+            View view = inflater.inflate(R.layout.demo_info_item, null);
+            MyHolder holder = new MyHolder(view);
+            return holder;
         }
 
         @Override
-        public int getCount() {
-            return demos.size();
+        public void onBindViewHolder(MyHolder holder, final int position) {
+            holder.title.setText(demos.get(position).activitys.getSimpleName());
+            holder.desc.setText(demos.get(position).desc);
+            holder.itemshell.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(mContext, demos.get(position).activitys);
+                    startActivity(intent);
+                }
+            });
         }
 
-        @Override
-        public Object getItem(int index) {
-            return demos.get(index);
-        }
 
         @Override
         public long getItemId(int id) {
             return id;
         }
 
-
-        class Holder {
-            TextView title, desc;
+        @Override
+        public int getItemCount() {
+            return demos.size();
         }
+
+
+        class MyHolder extends RecyclerView.ViewHolder {
+            TextView title, desc;
+            LinearLayout itemshell;
+
+            public MyHolder(View itemView) {
+                super(itemView);
+                title = V.f(itemView, R.id.title);
+                desc = V.f(itemView, R.id.desc);
+                itemshell = V.f(itemView, R.id.itemshell);
+            }
+        }
+
     }
 
 
