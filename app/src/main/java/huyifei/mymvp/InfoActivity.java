08701dcc76a6ed3fea61ba7ba733bed498ea0209
REBOOTERS <foreverlove.zyl@163.com>
2017-01-13 17:05:52 +0800

    handler test

--- app/src/main/java/huyifei/mymvp/InfoActivity.java
+++ app/src/main/java/huyifei/mymvp/InfoActivity.java
@@ -1,20 +1,49 @@
 package huyifei.mymvp;
 
 import android.app.ActivityManager;
+import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import huyifei.mymvp.util.V;
 
 public class InfoActivity extends AppCompatActivity {
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this;
         setContentView(R.layout.activity_info);
         TextView heapSizeTv = V.f(this, R.id.heapSizeTv);
         int heap_size = ActivityManager.MemoryInfo.CONTENTS_FILE_DESCRIPTOR;
-        heapSizeTv.setText(heap_size+"");
+        heapSizeTv.setText(heap_size + "");
+
+
+        //        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                Looper.prepare();
+//                Looper.loop();
+//
+//                try {
+//                    Thread.sleep(2000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                Toast.makeText(mContext, "Update UI !", Toast.LENGTH_SHORT).show();
+//            }
+//        }).start();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(mContext, "Update UI !", Toast.LENGTH_SHORT).show();
+            }
+        }, 1000);
     }
 }
