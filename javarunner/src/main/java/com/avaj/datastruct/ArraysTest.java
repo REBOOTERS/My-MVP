f8a022c8a9a7c99c4dd076b4ad20ee146fe2ea4c
REBOOTERS <foreverlove.zyl@163.com>
2017-08-16 15:49:56 +0800

    temp

---
+++ javarunner/src/main/java/com/avaj/datastruct/ArraysTest.java
@@ -0,0 +1,20 @@
+package com.avaj.datastruct;
+
+import java.util.Arrays;
+
+/**
+ * Created by Rookie on 2017/8/14.
+ */
+
+public class ArraysTest {
+    public static void main(String[] args) {
+        String[] datas = new String[]{"1111", "12323", "3343", "4343"};
+
+        Arrays.sort(datas);
+
+        int x=Arrays.binarySearch(datas, "1111");
+
+
+
+    }
+}
