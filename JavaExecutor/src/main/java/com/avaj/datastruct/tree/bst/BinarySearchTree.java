0914d83d77417c9ecb6a973e79f665d7cfde55a7
REBOOTERS <foreverlove.zyl@163.com>
2017-10-27 08:55:20 +0800

    bst tree

--- JavaExecutor/src/main/java/com/avaj/datastruct/tree/bst/BinarySearchTree.java
+++ JavaExecutor/src/main/java/com/avaj/datastruct/tree/bst/BinarySearchTree.java
@@ -12,6 +12,18 @@ public class BinarySearchTree<T extends Comparable<T>> {
     private TreeNode<T> root = null;
 
 
+    /**
+     * 树中插入元素
+     *
+     * @param value
+     */
+    void insert(T value) {
+        if (value == null) {
+            return;
+        }
+        root = insert(root, value);
+    }
+
     private TreeNode<T> insert(TreeNode<T> node, T value) {
         if (node == null) {
             return new TreeNode<>(null, value, null);
@@ -26,18 +38,93 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return node;
     }
 
-
-    public void insert(T value) {
-        if (value == null) {
-            return;
-        }
-        root = insert(root, value);
+    private boolean isEmpty() {
+        return root == null;
     }
 
     /**
-     * 打印树
+     * find 特定值 递归实现
+     *
+     * @param value
+     * @return
      */
-    public void printTree() {
+    public TreeNode<T> find(T value) {
+        if (isEmpty()) {
+            return null;
+        } else {
+            return find(root, value);
+        }
+    }
+
+    private TreeNode<T> find(TreeNode<T> node, T value) {
+        if (value.compareTo(node.getData()) < 0) {
+            return find(node.getLeftChild(), value);
+        } else if (value.compareTo(node.getData()) > 0) {
+            return find(node.getRightChild(), value);
+        } else {
+            return node;
+        }
+    }
+
+    /**
+     * 查找特定值-非递归实现
+     *
+     * @param value
+     * @return 结点
+     */
+    public TreeNode<T> findIter(T value) {
+        TreeNode<T> current = root;
+        while (current != null) {
+            if (value.compareTo(current.getData()) < 0) {
+                current = current.getLeftChild();
+            } else if (value.compareTo(current.getData()) > 0) {
+                current = current.getRightChild();
+            } else {
+                return current;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 查找最大值
+     *
+     * @return
+     */
+    public T findMax() {
+        if (isEmpty()) return null;
+
+        TreeNode<T> temp = root;
+        while (temp.getRightChild() != null) {
+            temp = temp.getRightChild();
+        }
+
+        return temp.getData();
+    }
+
+
+    /**
+     * 查找最小值
+     * @return
+     */
+    public T findMin() {
+        if (isEmpty()) return null;
+
+        TreeNode<T> temp = root;
+        while (temp.getLeftChild() != null) {
+            temp = temp.getLeftChild();
+        }
+
+        return temp.getData();
+    }
+
+
+
+
+    /**
+     * 以三种遍历方式打印树
+     */
+    void printTree() {
         System.out.print("\n前序遍历：");
         preTraversal(root);
         System.out.print("\n中序遍历：");
@@ -69,7 +156,9 @@ public class BinarySearchTree<T extends Comparable<T>> {
             preTraversal(node.getLeftChild());
             preTraversal(node.getRightChild());
         }
-    }/**
+    }
+
+    /**
      * 中序遍历-递归实现
      *
      * @param node
@@ -84,6 +173,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
 
     /**
      * 后序遍历-递归实现
+     *
      * @param node
      */
     void postTraversal(TreeNode node) {
--- JavaExecutor/src/main/java/com/avaj/datastruct/tree/bst/BinarySearchTreeTest.java
+++ JavaExecutor/src/main/java/com/avaj/datastruct/tree/bst/BinarySearchTreeTest.java
@@ -10,14 +10,30 @@ public class BinarySearchTreeTest {
     public static void main(String[] args) {
         BinarySearchTree<Integer> mSearchTree = new BinarySearchTree<>();
 
-        mSearchTree.insert(3);
+        mSearchTree.insert(8);
         mSearchTree.insert(2);
+        mSearchTree.insert(10);
         mSearchTree.insert(4);
-        mSearchTree.insert(6);
+        mSearchTree.insert(12);
+        //插入相同值测试
+        mSearchTree.insert(10);
         mSearchTree.insert(5);
         mSearchTree.insert(7);
         mSearchTree.insert(1);
+        mSearchTree.insert(17);
 
         mSearchTree.printTree();
+
+
+        System.out.printf("\nfind value %d in mSearchTree \n", 12);
+        TreeNode mTreeNode = mSearchTree.find(12);
+        TreeNode mTreeNode_1 = mSearchTree.findIter(12);
+        System.out.println("递归实现结点  = :" + mTreeNode+" value="+mTreeNode.getData());
+        System.out.println("非递归实现结点= :" + mTreeNode_1+" value="+mTreeNode_1.getData());
+
+        System.out.println("\nfind the max value in mSearchTree = " + mSearchTree.findMax());
+        System.out.println("find the min value in mSearchTree = " + mSearchTree.findMin());
+
     }
 }
+
