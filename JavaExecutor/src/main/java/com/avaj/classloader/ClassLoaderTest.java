04d1c1874f5ef5c661fc554a775fb55ab02b8b86
REBOOTERS <foreverlove.zyl@163.com>
2017-11-06 08:44:32 +0800

    class loader

---
+++ JavaExecutor/src/main/java/com/avaj/classloader/ClassLoaderTest.java
@@ -0,0 +1,35 @@
+package com.avaj.classloader;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Created by engineer on 2017/11/5.
+ */
+
+public class ClassLoaderTest {
+    private static final String LOCAL_PATH = "C:\\Users\\zyq\\Desktop\\javaLib";
+    private static final String CLASS_NAME = "Jobs";
+    public static void main(String[]args) {
+        LocalClassLoader mLocalClassLoader = new LocalClassLoader(LOCAL_PATH);
+        try {
+            Class c = mLocalClassLoader.findClass(CLASS_NAME);
+            if (c != null) {
+                Object object=c.newInstance();
+                System.out.println("ClassLoader=" + object.getClass().getClassLoader());
+                Method mMethod = c.getDeclaredMethod("say", null);
+                mMethod.invoke(object, null);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
