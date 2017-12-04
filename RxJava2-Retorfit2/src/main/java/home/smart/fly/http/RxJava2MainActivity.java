c3410f7a208ad8d495596ae03c1cc82d49b247f5
REBOOTERS <foreverlove.zyl@163.com>
2017-12-04 08:25:36 +0800

    RxJava2

--- RxJava2-Retorfit2/src/main/java/home/smart/fly/http/RxJava2MainActivity.java
+++ RxJava2-Retorfit2/src/main/java/home/smart/fly/http/RxJava2MainActivity.java
@@ -1,8 +1,6 @@
 package home.smart.fly.http;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,31 +8,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import home.smart.fly.http.OkHttpCache.OkHttpCacheActivity;
+import home.smart.fly.http.adapter.ItemInfo;
+import home.smart.fly.http.adapter.MyAdapter;
 
 public class RxJava2MainActivity extends AppCompatActivity {
+    @BindView(R2.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R2.id.list)
+    RecyclerView list;
+    @BindView(R2.id.fab)
+    FloatingActionButton fab;
     private Context mContext;
-    private RecyclerView recyclerView;
+
     private List<ItemInfo> demos = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mContext=this;
+        mContext = this;
         setContentView(R.layout.activity_rxjava2_main);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,76 +50,11 @@ public class RxJava2MainActivity extends AppCompatActivity {
 
 
         demos.add(new ItemInfo(R.string.app_name, OkHttpCacheActivity.class));
-
-
-        recyclerView = V.f(this, R.id.list);
-        MyAdapter myAdapter = new MyAdapter();
-        recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
-        recyclerView.setAdapter(myAdapter);
-    }
-    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyHolder> {
-
-
-        public MyAdapter() {
-
-        }
-
-        @Override
-        public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater inflater = getLayoutInflater();
-            View view = inflater.inflate(R.layout.demo_info_item, null);
-            MyHolder holder = new MyHolder(view);
-            return holder;
-        }
-
-        @Override
-        public void onBindViewHolder(MyHolder holder, final int position) {
-            holder.title.setText(demos.get(position).activitys.getSimpleName());
-            holder.desc.setText(demos.get(position).desc);
-            holder.itemshell.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(mContext, demos.get(position).activitys);
-                    startActivity(intent);
-                }
-            });
-        }
-
-
-        @Override
-        public long getItemId(int id) {
-            return id;
-        }
-
-        @Override
-        public int getItemCount() {
-            return demos.size();
-        }
-
-
-        class MyHolder extends RecyclerView.ViewHolder {
-            TextView title, desc;
-            LinearLayout itemshell;
-
-            public MyHolder(View itemView) {
-                super(itemView);
-                title = V.f(itemView, R.id.title);
-                desc = V.f(itemView, R.id.desc);
-                itemshell = V.f(itemView, R.id.itemshell);
-            }
-        }
-
+        MyAdapter myAdapter = new MyAdapter(demos);
+        list.setLayoutManager(new LinearLayoutManager(mContext));
+        list.setAdapter(myAdapter);
     }
 
 
-    private class ItemInfo {
-        private final int desc;
-        private final Class<? extends Activity> activitys;
-
-        public ItemInfo(int desc, Class<? extends Activity> demoClass) {
-            this.desc = desc;
-            this.activitys = demoClass;
-        }
-    }
 
 }
