1452962d2854d675027788d75b5f908da53a3875
REBOOTERS <foreverlove.zyl@163.com>
2017-12-25 22:02:29 +0800

    update hashmap activity

--- app/src/main/java/huyifei/mymvp/test/HashMapActivity.java
+++ app/src/main/java/huyifei/mymvp/test/HashMapActivity.java
@@ -3,7 +3,6 @@ package huyifei.mymvp.test;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,11 +28,6 @@ public class HashMapActivity extends AppCompatActivity implements View.OnClickLi
 
         mHashMap = new HashMap<>();
 
-        int hash = "name".hashCode() ^ "name".hashCode() >>> 16;
-
-        Log.e(TAG, "onCreate: hash=" + hash);
-
-        mHashMap.put("name", "mike");
 
         Button mPut = findViewById(R.id.put);
         mPut.setOnClickListener(this);
@@ -59,13 +53,17 @@ public class HashMapActivity extends AppCompatActivity implements View.OnClickLi
                 }
                 break;
             case R.id.get:
-                key = mKey.getText().toString().trim();
-                result = mHashMap.get(key);
-                mResult.setText(result);
+                if (!TextUtils.isEmpty(key)) {
+                    result = mHashMap.get(key);
+                    mResult.setText(result);
+                }
+
                 break;
             case R.id.remove:
-                result = mHashMap.remove(key);
-                Toast.makeText(this, result + " has been removed !", Toast.LENGTH_SHORT).show();
+                if (!TextUtils.isEmpty(key)) {
+                    result = mHashMap.remove(key);
+                    Toast.makeText(this, result + " has been removed !", Toast.LENGTH_SHORT).show();
+                }
                 break;
             default:
                 break;
