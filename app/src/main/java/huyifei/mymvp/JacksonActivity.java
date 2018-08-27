52f802cdba1f63e2d99ace86d40f133a045db5e8
REBOOTERS <foreverlove.zyl@163.com>
2018-08-27 11:50:03 +0800

    fix bugs

--- app/src/main/java/huyifei/mymvp/JacksonActivity.java
+++ app/src/main/java/huyifei/mymvp/JacksonActivity.java
@@ -1,7 +1,17 @@
 package huyifei.mymvp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.Nullable;
+
+import java8.util.Optional;
+import java8.util.function.Consumer;
 
 public class JacksonActivity extends AppCompatActivity {
 
@@ -9,5 +19,44 @@ public class JacksonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jackson);
+        String ssid=getWifiSSID();
+        TextView wifiText= findViewById(R.id.wifissid);
+        wifiText.setText("wifi SSID：" + ssid);
     }
+
+
+    @Nullable
+    private String getWifiSSID() {
+
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+
+        if (wifiManager != null && wifiManager.isWifiEnabled()) {
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            if (wifiInfo != null) {
+                NetworkInfo.DetailedState state = WifiInfo.getDetailedStateOf(wifiInfo.getSupplicantState());
+                if (state == NetworkInfo.DetailedState.CONNECTED || state == NetworkInfo.DetailedState.OBTAINING_IPADDR) {
+                    return wifiInfo.getSSID();
+                }
+            }
+        }
+
+//        Optional.ofNullable(wifiManager).ifPresent((WifiManager wifiManager1) -> {
+//            if (wifiManager1.isWifiEnabled()) {
+//                WifiInfo wifiInfo = wifiManager1.getConnectionInfo();
+//                Optional.ofNullable(wifiInfo).ifPresent((Consumer<WifiInfo>) (WifiInfo wifiInfo1) -> {
+//                    NetworkInfo.DetailedState detailedState = WifiInfo.getDetailedStateOf(wifiInfo1.getSupplicantState());
+//                    if (detailedState == NetworkInfo.DetailedState.CONNECTED || detailedState == NetworkInfo.DetailedState.OBTAINING_IPADDR) {
+//
+//                        return wifiInfo1.getSSID();
+//                    }
+//                });
+//            }
+//
+//
+//        });
+        return null;
+    }
+
+
 }
