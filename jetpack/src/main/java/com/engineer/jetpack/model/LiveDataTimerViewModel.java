7a87367348945db4962efab2b1023183d5896103
rookie <yingkongshi11@gmail.com>
2019-03-26 08:28:09 +0800

    view model

--- jetpack/src/main/java/com/engineer/jetpack/model/LiveDataTimerViewModel.java
+++ jetpack/src/main/java/com/engineer/jetpack/model/LiveDataTimerViewModel.java
@@ -18,20 +18,23 @@ public class LiveDataTimerViewModel extends ViewModel {
     private MutableLiveData<Long> mElapsedTime = new MutableLiveData<>();
 
     private Long mInitalTime;
+    private ScheduledExecutorService mScheduledExecutorService;
+    private Runnable mRunnable;
 
     public LiveDataTimerViewModel() {
         mInitalTime = SystemClock.elapsedRealtime();
-
-        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(1);
-        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                long newValue = (SystemClock.elapsedRealtime() - mInitalTime) / 1000;
-                mElapsedTime.postValue(newValue);
-            }
-        }, 0,1, TimeUnit.SECONDS);
+        mScheduledExecutorService = Executors.newScheduledThreadPool(1);
+        mRunnable = () -> {
+            long newValue = (SystemClock.elapsedRealtime() - mInitalTime) / 1000;
+            mElapsedTime.postValue(newValue);
+        };
 
     }
+
+    public void startRealTime() {
+        mScheduledExecutorService.scheduleAtFixedRate(mRunnable, 0, 1, TimeUnit.SECONDS);
+    }
+
 
     public MutableLiveData<Long> getElapsedTime() {
         return mElapsedTime;
--- jetpack/src/main/java/com/engineer/jetpack/ui/VMActivity.java
+++ jetpack/src/main/java/com/engineer/jetpack/ui/VMActivity.java
@@ -1,9 +1,7 @@
 package com.engineer.jetpack.ui;
 
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
@@ -24,20 +22,16 @@ public class VMActivity extends AppCompatActivity {
         mLiveDataTimerViewModel = ViewModelProviders.of(this).get(LiveDataTimerViewModel.class);
 
         subscribe();
+
+        findViewById(R.id.button).setOnClickListener(v -> mLiveDataTimerViewModel.startRealTime());
     }
 
     private void subscribe() {
-        Observer<Long> elapsedTimerObserver = new Observer<Long>() {
-            @Override
-            public void onChanged(@Nullable Long aLong) {
-                String value = String.valueOf(aLong);
-                mTimerText.setText(value);
+        mLiveDataTimerViewModel.getElapsedTime().observe(this, aLong -> {
+            String value = String.valueOf(aLong);
+            mTimerText.setText(value);
 
-                Log.e(TAG, "onChanged: aLong==" + aLong);
-
-            }
-        };
-
-        mLiveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimerObserver);
+            Log.e(TAG, "onChanged: aLong==" + aLong);
+        });
     }
 }
